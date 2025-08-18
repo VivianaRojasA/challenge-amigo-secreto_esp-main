2c9c587ca6670422829922f703f1966cbf39f12c
Viviana Rojas Aguilera <v.rojas.aguilera@gmail.com>
2025-08-18 16:56:43 -0400

    funcion Agregar Amigos

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+let cantidadAmigos = 0;
+let contador = 0;
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+//funcion vinculada al botón Añadir
+function agregarAmigo() {
+    let nameAmigo = document.getElementById('amigo').value;
+    if (nombresAmigos.includes(nameAmigo)) {
+        alert("Nombre repetido, Ingresa un nombre distinto");
+    } else if (nameAmigo == "" ){
+        alert("Ingresa un nombre válido");
+    }
+    else {
+        nombresAmigos.push(nameAmigo);
+    }
+    limpiarCaja();
+    return;
+}
+
+NnombresAmigos = nombresAmigos.length;
+console.log(contador);
+console.log(cantidadAmigos);
+console.log(nombresAmigos);
